e6f491374800a3606b62ab51805e778781cc92c6
Paul Tobin <tobster111@gmail.com>
2018-05-02 17:12:02 +0100

    Added Home/Navbar/Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
+
+import Navbar from "./components/layout/Navbar/Navbar";
+import Footer from "./components/layout/Footer/Footer";
+import Home from "./components/layout/Home/Home";
+import Register from "./components/auth/Register/Register";
+import Login from "./components/auth/Login/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My React App</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
