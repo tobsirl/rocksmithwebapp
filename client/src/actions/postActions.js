f9e7a23877f13d1d55b56763abf0abd3bc5cd28f
Paul Tobin <tobster111@gmail.com>
2018-06-13 10:41:15 +0100

    Added the CommetFeed/CommentForm/CommentItem component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
   ADD_POST,
   GET_ERRORS,
+  CLEAR_ERRORS,
   GET_POSTS,
   GET_POST,
   POST_LOADING,
@@ -10,6 +11,7 @@ import {
 
 // Add Post
 export const addPost = (postData) => (dispatch) => {
+  dispatch(clearErrors());
   axios
     .post('/api/posts', postData)
     .then((res) =>
@@ -110,8 +112,27 @@ export const removeLike = (id) => (dispatch) => {
 
 // Add Comment
 export const addComment = (postId, commentData) => (dispatch) => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
+    .then((res) =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => (dispatch) => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then((res) =>
       dispatch({
         type: GET_POST,
@@ -130,5 +151,12 @@ export const addComment = (postId, commentData) => (dispatch) => {
 export const setPostLoading = () => {
   return {
     type: POST_LOADING,
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
   };
 };
